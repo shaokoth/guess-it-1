3472a3c3a9261e8875e47daa6314e0379a589060
shaokoth <shadrackomondi005@gmail.com>
2024-08-15 12:13:25 +0300

    Fix issue with incorrect parsing of float values from stdin

--- go.mod
+++
@@ -1,3 +0,0 @@
-module guess-it
-
-go 1.22.2
--- student/mean.go
+++
@@ -1,9 +0,0 @@
-package stats
-
-func Average(input []float64) float64 {
-	sum := 0.0
-	for _, val := range input {
-		sum += val
-	}
-	return sum / float64(len(input))
-}
